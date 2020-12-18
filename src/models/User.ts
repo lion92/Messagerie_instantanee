59fe24f73510923f6b69537e24c43a6dc75ef974
Nicolas Kohler <nicolas.kohler@free.fr>
2020-12-18 10:08:31 +0100

    change user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,12 @@
 import { IUser } from '../interface/IUser';
 export default class User implements IUser{
 
-    private nom: string ="";
-    private prenom: string="";
-    private email: string="";
-    private password: string="";
+    private nom: string;
+    private prenom: string;
+    private email: string;
+    private password: string;
     private status: number=0;
-    private login: string="";
+    private login: string;
     private username: string="";
     
 
